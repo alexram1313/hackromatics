b1098e12d8a895ba97d2bafffa176f2931ad3501
Alex I. Ramirez <alex@arcompware.com>
2016-11-01 00:07:20 -0700

    Added UCI; Prints Stops and Vehicles per Route

--- example.py
+++ example.py
@@ -14,11 +14,12 @@ host = dict(
     delaware='http://udshuttle.com',            # Univ of Delaware
     sandiego='http://usdtram.com',              # Univ of San Diego
     nih='http://wttsshuttle.com/',              # NIH
-    ucsf='http://ucsfshuttles.com'              # UCSF
+    ucsf='http://ucsfshuttles.com',             # UCSF
+    uci='http://ucishuttles.com'                # UC Irvine
 )
 
 # connect to a syncromatics transit web site
-api = hackromatics.API(host['ucsf'])
+api = hackromatics.API(host['uci'])
 
 # to get info about the transit regions...
 regions = api.regions()
@@ -27,13 +28,13 @@ regions = api.regions()
 # in this ugly for loop...
 
 for region in api.regions():
-    print 'REGION:', region.ID, region.Name
+    print ('REGION:', region.ID, region.Name)
     for route in api.routes(region.ID):
-        print '\tROUTE:', route.ID, route.Name
-        print '\t\t', 'VEHICLES:'
-        for vehicle in api.vehicles(route.ID):
-            print '\t\t', vehicle.ID, vehicle.Name, \
-                        vehicle.Longitude, vehicle.Latitude
-        print '\t\t', 'BUS STOPS:'
-        for waypoint in api.waypoints(route.ID):
-            print '\t\t', waypoint.Longitude, waypoint.Latitude
+        print ('\tROUTE:', route.Name, '(R: '+str(route.ID)+')')
+        print('\t\tSTOPS:')
+        for stop in api.stops(route.ID):
+            print ('\t\t\t', stop.Name, '(S: '+str(stop.ID)+')')
+        print('\t\tVEHICLES:')
+        for veh in api.vehicles(route.ID):
+            print ('\t\t\t', veh.Latitude, ',', veh.Longitude, '(V: '+str(veh.Name)+')')
+        
